5b2651a99d147cd938311942d509a28a874c4dc6
Jonas Gann <jonas@gann.digital>
2021-12-30 14:01:04 +0100

    feat: change qr scanner resultDisplayDuration to 0

--- packages/cordova/src/CordovaScannerAccess.ts
+++ packages/cordova/src/CordovaScannerAccess.ts
@@ -15,7 +15,7 @@ export class CordovaScannerAccess implements INativeScannerAccess {
                     // torchOn: true, // Android, launch with the torch switched on (if available)
                     // saveHistory: true, // Android, save scan history (default false)
                     // prompt: "Place a barcode inside the scan area", // Android
-                    // resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
+                    resultDisplayDuration: 0, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
                     // formats: "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
                     // orientation: "landscape", // Android only (portrait|landscape), default unset so it rotates with the device
                     // disableAnimations: true, // iOS
