eb15624a966115127b3a70c0af284e8395ef2dc9
Jonas Gann <jonas@gann.digital>
2022-01-20 13:52:46 +0100

    Bugfix/jssnmshdd 2775 logging errors (#8)
    * feat: name file for default log "logs-default"
    
    * feat: fix loggger persistence
    
    * chore: comments

--- packages/common/src/CommonLoggerFactory.ts
+++ packages/common/src/CommonLoggerFactory.ts
@@ -4,50 +4,73 @@ import { Result } from "@js-soft/ts-utils";
 import Logger from "js-logger";
 
 export class CommonLoggerFactory implements INativeLoggerFactory {
-    public constructor(private readonly fileAccess: INativeFileAccess) {}
-
-    public init(): Promise<Result<void>> {
-        Logger.useDefaults({
-            defaultLevel: Logger.INFO
-        });
+    public constructor(private readonly fileAccess: INativeFileAccess) {
+        Logger.useDefaults({ defaultLevel: Logger.INFO });
 
         const consoleHandler = Logger.createDefaultHandler({
             formatter: function (messages, context) {
-                messages.unshift(`${new Date().toISOString()} [${context.name === "" ? "default" : context.name}]`);
+                messages.unshift(`${new Date().toISOString()} [${context.name === undefined ? "default" : context.name}]`);
             }
         });
-        this._initWeb(consoleHandler);
 
+        Logger.setHandler(async (messages: any[], context: any) => {
+            await this.addLog(context.name === undefined ? "default" : context.name, this.formatMessages(messages, context));
+            consoleHandler(messages, context);
+        });
+    }
+
+    public init(): Promise<Result<void>> {
         return Promise.resolve(Result.ok(undefined));
     }
 
     private formatMessages(messages: any[], context?: Function) {
         let message = "";
-
-        for (const item of messages) {
-            message += JSON.stringify(item);
-        }
-
+        for (const item of messages) message += JSON.stringify(item);
         return `${new Date().toISOString()} [${!context || !context.name ? "default" : context.name}] ${message}`;
     }
 
-    private _initWeb(consoleHandler: Function) {
-        const saveLogs = async (loggerName: string, message: string) => {
-            const loggerPath = `logs/logs-${loggerName}`;
+    private readonly queue: { loggerName: string; message: string }[] = [];
+    private busy = false;
 
-            const result = await this.fileAccess.infoFile(loggerPath);
-            if (result.isError) {
-                // file does not exist
-                await this.fileAccess.writeFile(loggerPath, `${message}\n`);
-            } else {
-                await this.fileAccess.writeFile(loggerPath, `${message}\n`, undefined, true);
+    private async saveQueue() {
+        this.busy = true;
+        while (this.queue.length) {
+            const log = this.queue.shift();
+            if (log) {
+                const loggerName = log.loggerName;
+                const message = log.message;
+                const date = new Date();
+                const day = date.getDay();
+                // Keep at most 7 versions of a log file, one for each day of the week
+                const loggerPath = `logs/logs_${loggerName}_${day}`;
+                const result1 = await this.fileAccess.infoFile(loggerPath);
+                if (result1.isError) {
+                    // file does not exist
+                    await this.fileAccess.writeFile(loggerPath, `${message}\n`);
+                } else {
+                    await this.fileAccess.writeFile(loggerPath, `${message}\n`, undefined, true);
+                }
+                // delete log file from 7 days ago
+                const removePath = `logs/logs_${loggerName}_${(day + 1) % 7}`;
+                const result2 = await this.fileAccess.infoFile(removePath);
+                if (!result2.isError) {
+                    await this.fileAccess.deleteFile(removePath);
+                }
             }
-        };
+        }
+        this.busy = false;
+    }
 
-        Logger.setHandler(async (messages: any[], context: any) => {
-            await saveLogs(context.name, this.formatMessages(messages, context));
-            consoleHandler(messages, context);
-        });
+    /**
+     * Add the log to a queue for saving to filesystem
+     * @param loggerName
+     * @param message
+     */
+    private async addLog(loggerName: string, message: string) {
+        // add log to queue
+        this.queue.push({ loggerName, message });
+        // save queue to fs if not already happening
+        if (!this.busy) await this.saveQueue();
     }
 
     public getLogger(oName: string | Function): ILogger {
@@ -57,9 +80,8 @@ export class CommonLoggerFactory implements INativeLoggerFactory {
         } else if (typeof oName === "string") {
             sName = oName;
         } else {
-            sName = "";
+            sName = "default";
         }
-
         return Logger.get(sName) as unknown as ILogger;
     }
 }
